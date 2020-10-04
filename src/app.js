bf2ea79f4f65f0125dfbd685ceb1d09c6cb83654
Charlie Jo <charliejames1652@gmail.com>
2020-10-04 12:03:46 -0400

    localhost linked up

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log("Hello!")
+const express = require("express")
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.send("My first express web page!!!!")
+
+})
+
+// app.get("/about" (req, res) => {
+//     res.send("My business is so cool!!!!")
+// })
+
+app.listen(3000, () => {
+    console.log("The server is now running on Port 3000")
+})
