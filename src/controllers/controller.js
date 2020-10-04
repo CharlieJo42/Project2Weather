efd358accf9a70f120e52bab2f8db4ba485e4a96
Charlie Jo <charliejames1652@gmail.com>
2020-10-04 14:42:40 -0400

    api key added; errors when using localhost

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,5 +1,18 @@
+const axios = require("axios")
+const API_KEY = "0b0424f8a2f7eb5f9c79a3066c2e0983"
+
 exports.renderHomePage = (req, res) => {
     res.render("index")
+}
+
+exports.getWeather = (req, res) => {
+    const city = req.body.city
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    axios.get(url).then((response) => {
+        console.log(response)
+    }).catch((error) => {
+        console.log(error)
+    })
 }
 
 exports.renderAboutPage = (req, res) => {
--- src/router.js
+++ src/router.js
@@ -4,8 +4,7 @@ const controller = require("./controllers/controller")
 
 
 router.get("/", controller.renderHomePage)
-
-
+router.post("/", controller.getWeather)
 router.get("/about", controller.renderAboutPage)
 
 module.exports = router
