d3fc2692d33897f0b73ab468cb6c66c5919d81b0
Charlie Jo <charliejames1652@gmail.com>
2020-10-05 07:42:28 -0400

    delete button added

--- client/src/components/ListCity.js
+++ client/src/components/ListCity.js
@@ -2,18 +2,35 @@ import React, { Fragment, useEffect, useState } from "react";
 
 const ListCity = () => {
 
+    const [city, setCity] = useState([]);
+
+    const deleteCity = async (id) => {
+        try {
+        const deleteCity = await fetch(`http://localhost:5000/city/${id}`, {
+            method: "DELETE"
+        });
+
+        setCity(city.filter(city => city.city_id !== id));
+        }catch (err) {
+            console.error(err.message)
+        }
+    }
+
     const getCity = async() => {
         try {
 
         const response = await fetch("http://localhost:5000/city")
-        } catch (err) {
+        const jsonData = await response.json();
+
+        setCity(jsonData);
+    } catch (err) {
             console.error(err.message);
         }
     }
 
     useEffect(() => {
         getCity();
-    });
+    }, []);
     return <Fragment><table class="table mt-5 text-center">
     <thead>
       <tr>
@@ -28,7 +45,13 @@ const ListCity = () => {
         <td>Doe</td>
         <td>john@example.com</td>
       </tr>*/}
-      
+      {city.map(city => (
+          <tr key={city.city_id}>
+              <td>{city.description}</td>
+              <td>Edit</td>
+              <td><button className="btn btn-danger" onCLick={() => deleteCity(city.city_id)}>Delete</button></td>
+          </tr>
+      ))}
     </tbody>
   </table></Fragment>
 };
