d64c91a96b4fff45ebae1c4e6387164832f9cce7
Charlie Jo <charliejames1652@gmail.com>
2020-10-05 07:19:12 -0400

    problems with localhost5000 connecting to react

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React, { Fragment } from 'react';
 import './App.css';
 
 import InputCity from "./components/InputCity";
+import ListCity from "./components/ListCity";
 
 function App() {
   return (
     <Fragment>
       <div className="container">
       <InputCity />
+      <ListCity />
       </div>
     </Fragment>
   );
---
+++ client/src/components/ListCity.js
@@ -0,0 +1,24 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+const ListCity = () => {
+    return <Fragment><table class="table mt-5 text-center">
+    <thead>
+      <tr>
+        <th>City Name</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+        {/* <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>*/}
+      
+    </tbody>
+  </table></Fragment>
+};
+
+export default ListCity;
+
